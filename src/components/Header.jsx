f8956084d625ac2bab67d83e045b1f7ee47ca47e
rubenaguilardev <ragui56@wgu.edu>
2025-06-07 15:02:11 -0700

    adjust font size for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,13 +16,13 @@ const Header = () => {
             <Nav />
             <main className="flex flex-1 items-center md:px-15 lg:px-22 xl:px-30 2xl:px-44">
                 <div>
-                    <h1 className="text-[#6633ee] pb-2 text-[.95rem] sm:text-xl lg:text-xl font-mono">Hi, nice to meet you. My name is</h1>
+                    <h1 className="text-[#6633ee] pb-2 text-md sm:text-xl lg:text-xl font-mono">Hi, nice to meet you. My name is</h1>
                     <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl
                     tracking-tighter font-semibold text-gray-900 pb-1">
                     Jazmin Buffington.
                     </h2>
                     <p className="bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text 
-                    text-[1.55rem] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl tracking-tight py-1
+                    text-[1.65rem] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl tracking-tight py-1
                     text-transparent font-semibold">I excel in {text}</p>
                     <p className="my-2 max-w-3xl pt-1 pb-9 text-lg sm:text-xl leading-relaxed tracking-tight text-gray-700">
                     {HERO_TEXT}</p>
