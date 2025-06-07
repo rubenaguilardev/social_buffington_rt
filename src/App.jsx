aa8a0321ba270259329471d8dc262bf065b3842d
rubenaguilardev <ragui56@wgu.edu>
2025-06-07 00:58:08 -0700

    adjust background gradient

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <div className="fixed inset-0 -z-10">
         <div className="min-h-screen w-full relative">
         <div className="absolute inset-0 z-0"
-          style={{background: "radial-gradient(125% 125% at 50% 90%, #fff 40%, #6366f1 100%)"}}/>
+          style={{background: "radial-gradient(125% 125% at 50% 90%, #fff 35%, #6366f1 100%)"}}/>
         </div>
       </div>
       <div className="px-6">
