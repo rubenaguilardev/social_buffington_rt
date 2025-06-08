ca7b174438433029599e0cc3ee6832cb35173b37
rubenaguilardev <ragui56@wgu.edu>
2025-06-08 03:36:38 -0700

    add skills section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                     text-[1.65rem] sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl tracking-tight py-1
                     text-transparent font-semibold">I excel in {text}</p>
                     <p className="my-2 max-w-3xl pt-1 pb-9 text-lg sm:text-xl leading-relaxed tracking-tight text-gray-700">
-                    {HERO_TEXT}{HERO_TEXT}{HERO_TEXT}</p>
+                    {HERO_TEXT}</p>
                     <button className="group relative h-12 cursor-pointer overflow-hidden overflow-x-hidden rounded-md 
                     bg-gray-900 px-8 py-2 text-gray-300 font-semibold font-['Inter']"><span className="relative z-10">
                     Resume</span><span className="absolute inset-0 overflow-hidden rounded-md">
