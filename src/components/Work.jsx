635629e371fb331aa193ed024198656eae41d8e2
rubenaguilardev <ragui56@wgu.edu>
2025-06-12 23:20:38 -0700

    styling update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,20 +11,28 @@ const Work = ({work}) => {
         if (section.id === 0) {
             return section.content.map((instagram, index) => (
                 <div key={index}>
-                    <img src={instagram.image}/>
-                    <p>{instagram.description}</p>
+                    <div>
+                        <img src={instagram.image}/>
+                        <p>{instagram.description}</p>
+                    </div>
                 </div>
             ))
         } else if (section.id === 1) {
             return section.content.map((img, index) => (
-                <img key={index} src={img} className='w-[48%] rounded-2xl'/>)
+                <img key={index} src={img} className='w-2/3 rounded-2xl shadow-xl'/>)
         )} else {
-            return section.content.map((blog, index) => (
-                <div key={index}>
-                    <img src={blog.image}/>
-                    <p>{blog.description}</p>
+            return (
+                <div className="flex flex-nowrap gap-6 overflow-x-auto pb-4">
+                    {section.content.map((blog, index) => (
+                    <div key={index} className="inline-flex flex-col w-[70vw] max-w-md shrink-0 rounded-b-lg
+                    bg-white shadow-lg p-4 text-center">
+                        <img src={blog.image} className="w-full h-auto object-cover rounded-md mb-3"
+                        alt={`blog-${index}`}/>
+                        <a href='#'><p className="text-gray-700 text-sm">{blog.description}</p></a>
+                    </div>
+                    ))}
                 </div>
-            ))
+            )
         }
     }
 
@@ -37,19 +45,22 @@ const Work = ({work}) => {
                 <div className="flex-1 h-px bg-[#00519c]"></div>
             </div>
         </section>
-        <div className='flex flex-wrap'>
-            {work.map((type, index) => (
-                <button 
-                    key={index}
-                    className={`py-3 px-6 font-medium text-sm duration-300 cursor-pointer ${activeTab === index ?
-                    "border-b-2 border-custom text-gray-300 bg-gray-900" : 
-                    "text-gray-400 hover:text-gray-300 hover:bg-custom hover:bg-opacity-10"} `}
-                    onClick={() => setActiveTab(index)}>
-                    {type.label}
-                </button>
-            ))}
+        <div className='flex justify-center border rounded-t-lg bg-gray-900'>
+            <div className='flex flex-wrap'>
+                {work.map((type, index) => (
+                    <button 
+                        key={index}
+                        className={`py-3 px-6 font-medium text-sm duration-300 cursor-pointer ${activeTab === index ?
+                        "border-b-4 border-custom text-gray-300 bg-gray-900" : 
+                        "text-gray-500 hover:text-gray-300 hover:bg-custom hover:bg-opacity-10"} `}
+                        onClick={() => setActiveTab(index)}>
+                        {type.label}
+                    </button>
+                ))}
+            </div>
         </div>
-        <div>{renderContent(work[activeTab])}</div>
+        <div className='flex flex items-center gap-12 overflow-visible'>{renderContent(work[activeTab])}</div>
+        
     </section>
     )
 }
