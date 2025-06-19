22e33331725de222cdc6d3996f07a1335d035735
rubenaguilardev <ragui56@wgu.edu>
2025-06-19 02:17:11 -0700

    updates

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,12 +4,13 @@ import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/free-mode'
 import {FreeMode, Pagination} from 'swiper/modules'
-import {RxArrowTopRight} from 'react-icons/rx'
+
 
 const Testimonials = () => {
 
     return (
-        <section className="flex items-center justify-center flex-col h-screen bg[#6c34af]">
+        <section className="flex items-center justify-center flex-col my-40">
+            <h2 className="text-4xl font-semibold mb-10 text-gray-900 text-center">Check out what my colleagues have to say</h2>
             <Swiper
                 slidesPerView="auto"
                 spaceBetween={15}
@@ -20,7 +21,7 @@ const Testimonials = () => {
             >
                 {TESTIMONIAL_CONTENT.map((test, index) => (
                     <SwiperSlide key={index} className="!w-[415px] lg:!w-[450px]">
-                        <div className="flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8
+                        <div className="flex flex-col group relative shadow-lg text-white rounded-xl px-5 py-8
                         h-[450px] lg:h-[500px] cursor-pointer mb-20">
                             <div className="absolute inset-0 bg-cover bg-center"
                             style={{ backgroundImage: `url(${test.background})` }} />
@@ -30,12 +31,10 @@ const Testimonials = () => {
                                 <h3 className="text-lg lg:text-xl">{test.company}</h3>
                                 <p className="lg:text-[18px]">{test.testimonial}</p>
                             </div>
-                            <RxArrowTopRight className="absolute bottom-5 left-5 w-[35px] h-[35px] text-white 
-                            group-hover:text-blue-500 group-hover:rotate-45 duration-100" />
                         </div>
                     </SwiperSlide>
                 ))}
-            </Swiper>
+                </Swiper>
         </section>
     )
 }
